cec91eb88b3f06f306de97972eaa9e119247fe79
FudgyDRS <yetimonsters@gmail.com>
2023-10-22 08:57:31 -0700

    so far

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+import noImg from '../images/no-image-icon.png';
+
+export function getData(){
+    return [
+        { title: "Pizza", price: 17.99, Image: noImg },
+        { title: "Burger", price: 15, Image: noImg },
+        { title: "Coke", price: 3.5, Image: noImg },
+        { title: "Kebab", price: 13.99, Image: noImg },
+        { title: "Salad", price: 2.5, Image: noImg },
+        { title: "Bottle of Water", price: 0.99, Image: noImg },
+        { title: "Ice Cream", price: 2.99, Image: noImg }
+    ]
+}
